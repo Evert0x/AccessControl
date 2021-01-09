9f8e3dd2e583e5b4fd4e5dbef2866eebca028a09
Evert Kors <evert@hatching.io>
2021-01-09 14:20:08 +0100

    Simplify structure

--- contracts/AccessControl.sol
+++ contracts/AccessControl.sol
@@ -7,10 +7,9 @@ import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "./storage/LibAccessStorage.sol";
 import "./interfaces/IAccessControl.sol";
-import "./AccessControlLib.sol";
 import "./RelayReceiver.sol";
 
-contract AccessControl is AccessControlLib, IAccessControl {
+contract AccessControl is IAccessControl, RelayReceiver {
     using EnumerableSet for EnumerableSet.AddressSet;
     using Address for address;
 
@@ -78,5 +77,47 @@ contract AccessControl is AccessControlLib, IAccessControl {
             "AccessControl: can only renounce roles for self"
         );
         _revokeRole(role, account);
+    }
+
+    function _hasRole(bytes32 role, address account)
+        internal
+        virtual
+        view
+        returns (bool)
+    {
+        LibAccessStorage.RoleStorage storage rs = LibAccessStorage
+            .roleStorage();
+
+        return rs.roles[role].members.contains(account);
+    }
+
+    function _setupRole(bytes32 role, address account) internal virtual {
+        _grantRole(role, account);
+    }
+
+    function _setRoleAdmin(bytes32 role, bytes32 adminRole) internal virtual {
+        LibAccessStorage.RoleStorage storage rs = LibAccessStorage
+            .roleStorage();
+
+        emit RoleAdminChanged(role, rs.roles[role].adminRole, adminRole);
+        rs.roles[role].adminRole = adminRole;
+    }
+
+    function _grantRole(bytes32 role, address account) internal virtual {
+        LibAccessStorage.RoleStorage storage rs = LibAccessStorage
+            .roleStorage();
+
+        if (rs.roles[role].members.add(account)) {
+            emit RoleGranted(role, account, _msgSender());
+        }
+    }
+
+    function _revokeRole(bytes32 role, address account) internal virtual {
+        LibAccessStorage.RoleStorage storage rs = LibAccessStorage
+            .roleStorage();
+
+        if (rs.roles[role].members.remove(account)) {
+            emit RoleRevoked(role, account, _msgSender());
+        }
     }
 }
