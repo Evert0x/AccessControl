1fd0fbee96fc3438bf85a7cc5e3a2017562ef782
Evert Kors <evert@hatching.io>
2021-01-09 14:18:20 +0100

    Remove unused imports

--- contracts/storage/LibAccessStorage.sol
+++ contracts/storage/LibAccessStorage.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.7.4;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/EnumerableSet.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
 
 library LibAccessStorage {
     bytes32 constant ACCESS_STORAGE_POSITION = keccak256(
